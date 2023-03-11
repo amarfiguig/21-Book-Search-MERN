509aff34118b90b73991c90ed33f0df364277860
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-11 21:00:02 +0000

    Update SignupForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,52 +1,52 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-/// new line
-import { useMutation } from '@apollo/client';
-// Import the GraphQL mutation
+import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
-// new line end
-
-//import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
-  // set initial form state
+  // Set up state for user form data, validation, and error alert
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  // set state for form validation
-  const [validated] = useState(false);
-  // set state for alert
+  const [validated, setValidated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  // new lines
-  // Invoke `useMutation()` hook to return a Promise-based function and data about the ADD_PROFILE mutation
-  const [addUser] = useMutation(ADD_USER);
-  //console.log(error);
-  // new line end
 
+  // Define mutation for adding user
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  // Function for handling changes to user form data
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  // Function for handling form submission
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
+    // Check form validation
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
-    
+
     try {
+      // Add user with form data
       const { data } = await addUser({
         variables: { ...userFormData },
       });
+      console.log(data);
       
+      // Log user in with authentication token
       Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
+    } catch (err) {
+      // Display error alert
+      console.error(err);
+      setShowAlert(true);
     }
+
+    // Reset form data
     setUserFormData({
       username: '',
       email: '',
@@ -56,13 +56,13 @@ const SignupForm = () => {
 
   return (
     <>
-      {/* This is needed for the validation functionality above */}
+      {/* Form with validation and error alert */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/* show alert if server response is bad */}
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your signup!
         </Alert>
 
+        {/* Username field */}
         <Form.Group>
           <Form.Label htmlFor='username'>Username</Form.Label>
           <Form.Control
@@ -76,6 +76,7 @@ const SignupForm = () => {
           <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
         </Form.Group>
 
+        {/* Email field */}
         <Form.Group>
           <Form.Label htmlFor='email'>Email</Form.Label>
           <Form.Control
@@ -89,6 +90,7 @@ const SignupForm = () => {
           <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
         </Form.Group>
 
+        {/* Password field */}
         <Form.Group>
           <Form.Label htmlFor='password'>Password</Form.Label>
           <Form.Control
@@ -101,6 +103,8 @@ const SignupForm = () => {
           />
           <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
         </Form.Group>
+
+        {/* Submit button */}
         <Button
           disabled={!(userFormData.username && userFormData.email && userFormData.password)}
           type='submit'
