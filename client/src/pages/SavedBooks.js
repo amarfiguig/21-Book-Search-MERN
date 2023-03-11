f46cab045eafb1127c2286458083dfb4b22775fb
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-11 20:39:32 +0000

    Update SavedBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,76 +1,75 @@
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-// new lines
-import { useMutation, useQuery } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
-/// new lines end
-//import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
-
-
+import { useQuery, useMutation } from '@apollo/react-hooks'; // Importing hooks for querying and mutating data using Apollo Client
+import { QUERY_ME } from '../utils/queries'; // Importing query for fetching user data
+import { REMOVE_BOOK } from '../utils/mutations'; // Importing mutation for removing a book from the saved books list
+import Auth from '../utils/auth'; // Importing helper function for checking if user is logged in
+import { removeBookId } from '../utils/localStorage'; // Importing function for removing a book ID from local storage
 
 const SavedBooks = () => {
-  
-  //const [userData, setUserData] = useState({});
-  const [removeBook] = useMutation(REMOVE_BOOK);
+  const { loading, data } = useQuery(QUERY_ME); // Querying the logged in user's data
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK); // Mutation for removing a book from the saved books list
 
-  const { loading, data } = useQuery(GET_ME);
-//console.log(data);
-  var userData = data?.me.savedBooks || [];
-  //setUserData(userD);
+  const userData = data?.me || {}; // Extracting the user data object from the query response, or setting it to an empty object
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const userDataLength = Object.keys(userData).length; // Getting the number of keys in the user data object
+
   const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    const token = Auth.loggedIn() ? Auth.getToken() : null; // Getting the user's auth token if they are logged in
 
     if (!token) {
-      return false;
+      return false; // If user is not logged in, do not proceed with deleting the book and return false
     }
 
     try {
-      const { loading, data } = await removeBook({
-        variables: {bookId },token
+      const { data } = await removeBook({ // Calling the removeBook mutation with the bookId as the argument
+        variables: { bookId },
       });
-       userData = data?.data || [];
-    // setUserData(Duser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
+
+      removeBookId(bookId); // Removing the book ID from local storage
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  //if (!userDataLength) {
-  if (loading) {
+  if (loading) { // If data is still loading, display a loading message
     return <h2>LOADING...</h2>;
   }
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className='text-light bg-light'>
         <Container>
-          <h1>Viewing saved books!</h1>
+          <h1>Viewing {userData.username}'s saved books!</h1> // Displaying the logged in user's username
         </Container>
       </Jumbotron>
       <Container>
         <h2>
-          {userData.length
-            ? `Viewing ${userData.length} saved ${userData.length === 1 ? 'book' : 'books'}:`
+          {userData.savedBooks?.length // Checking if the user has any saved books
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? 'book' : 'books'
+              }:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.map((book) => {
+          {userData.savedBooks?.map((book) => { // Mapping over the saved books array and rendering a Card component for each book
             return (
               <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant='top'
+                  />
+                ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button
+                    className='btn-block btn-danger'
+                    onClick={() => handleDeleteBook(book.bookId)} // Calling the handleDeleteBook function with the bookId as the argument
+                  >
                     Delete this Book!
                   </Button>
                 </Card.Body>
