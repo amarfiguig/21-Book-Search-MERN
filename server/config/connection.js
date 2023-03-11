327bed2f93ac7cfccf7c806c7020d3847db2abfd
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-11 21:15:49 +0000

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksearch', { // "but-your-database-name booksearch just example"
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
