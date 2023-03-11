2c2aef39f78267ebb838f5abbdef918502811535
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-11 21:36:04 +0000

    Update resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,71 +1,95 @@
+// Import necessary packages
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
+// Define the resolvers object
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find();
-    },
-    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
     me: async (parent, args, context) => {
+      // Check if the user is authenticated
       if (context.user) {
-        return User.findOne({ _id: context.user._id });
+        // Find the user data and remove some fields from the response
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+        return userData;
       }
-      throw new AuthenticationError('You need to be logged in!');
+
+      // Throw an error if the user is not authenticated
+      throw new AuthenticationError('Not logged in');
     },
   },
 
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+    addUser: async (parent, args) => {
+      console.log(args);
+      // Create a new user
+      const user = await User.create(args);
+      // Generate a JWT token for the user
       const token = signToken(user);
 
+      // Return the token and the user data
       return { token, user };
     },
-    loginUser: async (parent, { email, password }) => {
+    login: async (parent, { email, password }) => {
+      // Find the user with the given email
       const user = await User.findOne({ email });
 
+      // Throw an error if the user does not exist
       if (!user) {
-        throw new AuthenticationError('No user with this email found!');
+        throw new AuthenticationError('Incorrect credentials');
       }
 
+      // Check if the password is correct
       const correctPw = await user.isCorrectPassword(password);
 
+      // Throw an error if the password is incorrect
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect password!');
+        throw new AuthenticationError('Incorrect credentials');
       }
 
+      // Generate a JWT token for the user
       const token = signToken(user);
+
+      // Return the token and the user data
       return { token, user };
     },
-
-    // Add a third argument to the resolver to access data in our `context`
     saveBook: async (parent, { bookData }, context) => {
-      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      // Check if the user is authenticated
       if (context.user) {
-        return User.findByIdAndUpdate(
+        // Update the user's saved books array by adding the new book data
+        const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
           { $push: { savedBooks: bookData } },
           { new: true }
         );
+
+        // Return the updated user data
+        return updatedUser;
       }
-      // If user attempts to execute this mutation and isn't logged in, throw an error
+
+      // Throw an error if the user is not authenticated
       throw new AuthenticationError('You need to be logged in!');
     },
-    
-    // Make it so a logged in user can only remove a book from their own saveBooks
     removeBook: async (parent, { bookId }, context) => {
+      // Check if the user is authenticated
       if (context.user) {
-        return User.findOneAndUpdate(
+        // Update the user's saved books array by removing the book with the given ID
+        const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { savedBooks: { bookId: bookId } } },
+          { $pull: { savedBooks: { bookId } } },
           { new: true }
         );
+
+        // Return the updated user data
+        return updatedUser;
       }
+
+      // Throw an error if the user is not authenticated
       throw new AuthenticationError('You need to be logged in!');
     },
   },
 };
 
+// Export the resolvers object
 module.exports = resolvers;
