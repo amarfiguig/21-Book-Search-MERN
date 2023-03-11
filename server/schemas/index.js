ce2f5091c20c68b7a895eddcb147f1b9d1a791c2
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-11 21:30:09 +0000

    Update index.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,6 @@
+// Importing type definitions and resolvers
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
+// Exporting type definitions and resolvers as an object
 module.exports = { typeDefs, resolvers };
